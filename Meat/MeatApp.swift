04d90e5116fbe56af801d1b5dfbd6550556e02b7
Shakshi3104 <watchosastrom3104@gmail.com>
2021-07-12 10:02:19 +0900

    Add 🥩 budge

--- Meat/MeatApp.swift
+++ Meat/MeatApp.swift
@@ -9,9 +9,30 @@ import SwiftUI
 
 @main
 struct MeatApp: App {
+    @UIApplicationDelegateAdaptor(AppDelegate.self) var appDelegate
+    
     var body: some Scene {
         WindowGroup {
             ContentView()
         }
     }
+}
+
+class AppDelegate: UIResponder, UIApplicationDelegate {
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions lanchOptions: [UIApplication.LaunchOptionsKey: Any]? = nil) -> Bool {
+            let center = UNUserNotificationCenter.current()
+            
+            center.requestAuthorization(options: [.badge]) { granted, error in
+                if let error = error {
+                    // Handle the error
+                    print(error)
+                }
+                
+                DispatchQueue.main.async {
+                    UIApplication.shared.perform(NSSelectorFromString("setApplicationBadgeString:"), with: "🥩")
+                }
+            }
+            
+            return true
+        }
 }
